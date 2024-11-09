5c94ab2e1d20f307293578134c23673962eecc07
Omid Nikrah <omidnikrah@gmail.com>
2024-11-09 23:59:19 +0330

    ✨ feat: handle socket exceptions inside useSocket hook and return errors

--- apps/frontend/src/hooks/useSocket.ts
+++ apps/frontend/src/hooks/useSocket.ts
@@ -1,19 +1,25 @@
 import { socket } from '@gol-ya-pooch/frontend/services';
+import { Events } from '@gol-ya-pooch/shared';
 import { useEffect, useState } from 'react';
 
 export const useSocket = () => {
   const [isConnected, setIsConnected] = useState(socket.connected);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     const handleConnect = () => setIsConnected(true);
     const handleDisconnect = () => setIsConnected(false);
+    const handleGameException = (err: { status: string; message: string }) =>
+      setError(err.message);
 
     socket.on('connect', handleConnect);
     socket.on('disconnect', handleDisconnect);
+    socket.on(Events.GAME_EXCEPTION, handleGameException);
 
     return () => {
       socket.off('connect', handleConnect);
       socket.off('disconnect', handleDisconnect);
+      socket.off(Events.GAME_EXCEPTION);
     };
   }, []);
 
@@ -29,5 +35,5 @@ export const useSocket = () => {
     socket?.off(event);
   };
 
-  return { isConnected, emit, on, off };
+  return { isConnected, emit, on, off, error };
 };
