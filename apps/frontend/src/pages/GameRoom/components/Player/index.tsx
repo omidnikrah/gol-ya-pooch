f5d9e6ee52c5df00676b3a6a44daf92c68c75751
Omid Nikrah <omidnikrah@gmail.com>
2024-11-11 21:09:18 +0330

    ✨ feat: add hands of teamB and handle Player component to render releavent hands

--- apps/frontend/src/pages/GameRoom/components/Player/index.tsx
+++ apps/frontend/src/pages/GameRoom/components/Player/index.tsx
@@ -1,11 +1,25 @@
+import { TeamNames } from '@gol-ya-pooch/shared';
 import Spline from '@splinetool/react-spline';
+import clsx from 'clsx';
 
-export const Player = () => {
+interface IPlayer {
+  team: TeamNames;
+}
+
+export const Player = ({ team }: IPlayer) => {
   return (
-    <div className="w-full h-1/2 rounded-tr-full rounded-tl-full bg-purple-80 aspect-square relative flex items-end">
+    <div
+      className={clsx('w-full h-1/2 bg-purple-80 aspect-square relative flex', {
+        'rounded-tr-full rounded-tl-full items-end': team === 'teamA',
+        'rounded-br-full rounded-bl-full items-start': team === 'teamB',
+      })}
+    >
       <Spline
-        scene="/models/blue-team-hands.splinecode"
-        className="absolute flex justify-center !h-[120px] translate-y-6"
+        scene={`/models/${team === 'teamA' ? 'blue' : 'red'}-team-hands.splinecode`}
+        className={clsx('absolute flex justify-center !h-[120px]', {
+          'translate-y-6': team === 'teamA',
+          '-translate-y-6': team === 'teamB',
+        })}
       />
     </div>
   );
--- apps/frontend/src/pages/GameRoom/index.tsx
+++ apps/frontend/src/pages/GameRoom/index.tsx
@@ -29,16 +29,16 @@ const GameRoomPage = () => {
   return (
     <div className="flex justify-center">
       <div className="flex justify-center relative max-w-[1000px]">
-        <div className="w-[80%] absolute top-0 grid grid-cols-3 gap-10 items-end rotate-180">
-          <Player />
-          <Player />
-          <Player />
+        <div className="w-[80%] absolute top-0 grid grid-cols-3 gap-10 items-start">
+          <Player team="teamB" />
+          <Player team="teamB" />
+          <Player team="teamB" />
         </div>
         <GameTableIcon className="w-full h-auto" />
         <div className="w-[90%] absolute bottom-0 grid grid-cols-3 gap-10 items-end">
-          <Player />
-          <Player />
-          <Player />
+          <Player team="teamA" />
+          <Player team="teamA" />
+          <Player team="teamA" />
         </div>
       </div>
     </div>
